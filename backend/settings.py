182d3278361cfcaec0b34d3971d637b9ffe3a699
sylar123abc <sylar123abc@gmail.com>
2023-06-01 13:25:47 +0200

    Fix typo

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "api.apps.apiConfig",
+    "api.apps.ApiConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

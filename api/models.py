dc9f43a33330a8535ac6c08f1f1c78ad143fa1a7
sylar123abc <sylar123abc@gmail.com>
2023-06-02 12:20:57 +0200

    adding decimal precision settings

--- api/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Order (models.Model):
-    """The main class for the user orders being stored into the database"""
--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import decimal
 from pathlib import Path
+
+decimal.DefaultContext.prec = 200
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

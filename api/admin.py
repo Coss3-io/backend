ecf3110770c5327b2c3e231ef4d3f9909df6f087
sylar123abc <sylar123abc@gmail.com>
2023-06-07 11:12:44 +0200

    Registering the models in the admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models.order import Maker, ReplaceMaker, Taker, ReplaceTaker
 
 # Register your models here.
+admin.site.register(Maker)
+admin.site.register(ReplaceMaker)
+admin.site.register(Taker)
+admin.site.register(ReplaceTaker)

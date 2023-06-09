a2d72e76a4ed2366a0c93e6e02a3be98416bc6fa
sylar123abc <sylar123abc@gmail.com>
2023-06-09 11:31:57 +0200

    deleting useless maxlength validator

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.core.validators import MinLengthValidator
 
 
 class Maker(models.Model):

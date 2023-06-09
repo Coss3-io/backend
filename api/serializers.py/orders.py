9a1075e827dff213f7e2ab09d03ba86731e7ab99
sylar123abc <sylar123abc@gmail.com>
2023-06-09 11:24:56 +0200

    creating the taker order serializers

--- api/serializers.py/orders.py
+++ api/serializers.py/orders.py
@@ -1,16 +1,31 @@
 from rest_framework import serializers
-from models.orders import Maker
+from models.orders import Maker, Taker
 from models.types import MakerTypedDict
 from rest_framework.validators import ValidationError
 from eth_account import Account
 from eth_abi.packed import encode_packed
 
 
+class MakerListSerializer(serializers.ListSerializer):
+    """Used for multiple operation on orders, especially creation, and update"""
+
+    def create(self, validated_data):
+        makers = [Maker(**data) for data in validated_data]
+        return Maker.objects.bulk_create(makers)
+
+    def update(self, validated_data):
+        return Maker.objects.bulk_update(
+            [Maker(**data) for data in validated_data], ["filled", "status"]
+        )
+
+
 class MakerSerializer(serializers.ModelSerializer):
     """The maker order class serializer"""
+    id = serializers.IntegerField()
 
     class Meta:
         model = Maker
+        list_serializer_class = MakerListSerializer
 
     def validate(self, data: MakerTypedDict):
         message = encode_packed(
@@ -44,8 +59,25 @@ class MakerSerializer(serializers.ModelSerializer):
             ],
         )
         if (
-            Account.recover_message(f'"\x19Ethereum Signed Message:\n32"{message}', signature=data["signature"])
+            Account.recover_message(
+                f'"\x19Ethereum Signed Message:\n32"{message}',
+                signature=data["signature"],
+            )
             != data["owner"]
         ):
             raise ValidationError("The signature sent doesn't match the order owner")
         return super().validate(data)
+
+class TakerListSerializer(serializers.ListSerializer):
+    """List serializer for batch creation of taker orders"""
+
+    def create(self, validated_data):
+        takers = [Taker(**order) for order in validated_data]
+        return Taker.objects.bulk_create(takers)
+
+class TakerSerializer(serializers.ModelSerializer):
+    """Serializer used for the taker orders creation and deletetion"""
+
+    class Meta:
+        model = Maker
+        list_serializer_class = TakerListSerializer

eb3279de987db93e2ee26b9fbbf93665e81ea3c1
sylar123abc <sylar123abc@gmail.com>
2023-06-12 12:21:36 +0200

    Adding order hash and signature type

--- api/models/types.py
+++ api/models/types.py
@@ -1,16 +1,37 @@
 from typing import TypedDict, Any
 from decimal import Decimal
 from datetime import date
+from rest_framework.exceptions import APIException
 
 
 class Address(str):
     def __new__(cls, value):
         if len(value) > 42:
-            raise ValueError("The address you gave is too long")
+            raise APIException("the address you gave is too long")
         try:
             int(value, 0)
         except ValueError:
-            raise ValueError("The address submitted is hill formed")
+            raise APIException("the address submitted is hill formed")
+        return cls.__init__(value)
+    
+class Signature(str):
+    def __new__(cls, value):
+        if len(value) > 132:
+            raise APIException("the signature you gave is too long")
+        try:
+            int(value, 0)
+        except ValueError:
+            raise APIException("the signature submitted is hill formed")
+        return cls.__init__(value)
+    
+class KeccakHash(str):
+    def __new__(cls, value):
+        if len(value) > 66:
+            raise APIException("the hash you gave is too long")
+        try:
+            int(value, 0)
+        except ValueError:
+            raise APIException("the hash submitted is hill formed")
         return cls.__init__(value)
 
 

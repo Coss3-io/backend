9a23b5ef65f8ad93d4b705ec4f906d1913c4e33c
sylar123abc <sylar123abc@gmail.com>
2023-06-13 13:28:19 +0200

    adding the app name

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from api.views.orders import OrderView, MakerView
 
+app_name = "api"
 urlpatterns = [
     path("orders", OrderView.as_view(), name="orders"),
-    path("order", MakerView.as_view(), name="order")
+    path("order", MakerView.as_view(), name="order"),
 ]

81723f25368f3a10d019ce91ddcf334538d95985
sylar123abc <sylar123abc@gmail.com>
2023-06-13 12:46:23 +0200

    removing useless url slash

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api.views.orders import OrderView, MakerView
 
 urlpatterns = [
-    path("/orders", OrderView.as_view(), name="orders"),
-    path("/order", MakerView.as_view(), name="order")
+    path("orders", OrderView.as_view(), name="orders"),
+    path("order", MakerView.as_view(), name="order")
 ]

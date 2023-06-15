5f1bdaf73f2d997e24c0a76f9678320201dd01bb
sylar123abc <sylar123abc@gmail.com>
2023-06-15 12:26:14 +0200

    making updates on the test order files

--- api/tests/test_orders.py
+++ api/tests/test_orders.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from time import time
+from datetime import datetime
 from django.urls import reverse
 from rest_framework import status
 from api.models import User
@@ -20,12 +21,12 @@ class MakerOrderTestCase(APITestCase):
 
         data = {
             "amount": '{0:f}'.format(Decimal("173e16")),
-            "expiry": "9999999999999999",
+            "expiry": "1696667304",
             "price": '{0:f}'.format(Decimal("2e20")),
             "base_token": "0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520",
             "quote_token": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-            "signature": "0x796ee451cdc0b004e494e833a53e6c26b136696bce03a8dc63e2d7df73c09c3f03195e4021cad50bac726364258e024f6f6483eae8fd7b3da5b080717aab24b41c",
-            "order_hash": "0x73a4fa8382a91dbeadf8dfaca90219db7541a1955937e7c5529ae7e96788a187",
+            "signature": "0xfabfac7f7a8bbb7f87747c940a6a9be667a57c86c145fd2bb91d8286cdbde0253e1cf2c95bdfb87a46669bc8ba0d4f92b4786d00df7f90aea8004d2b953b27cb1b",
+            "order_hash": "0x0e3c530932af2cadc56e2cb633b4a4952b5ebb74888c19e1068c2d0213953e45",
             "is_buyer": False,
         }
         response = self.client.post(reverse("api:order"), data=data)

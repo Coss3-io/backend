68a1c94417141f0b2fedef3b5d39569dc8e28fbf
sylar123abc <sylar123abc@gmail.com>
2023-06-16 13:00:56 +0200

    Adding user creation signature related tests

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -22,7 +22,6 @@ class UserCreationTestCase(APITestCase):
             self.url,
             data={"signature": signature, "address": address, "timestamp": timestamp},
         )
-
         self.assertEqual(
             response.status_code, HTTP_200_OK, "The user creation should not fail"
         )
@@ -143,7 +142,7 @@ class UserCreationTestCase(APITestCase):
         self.assertEqual(
             response.status_code,
             HTTP_400_BAD_REQUEST,
-            "The request with a short address shoul fail",
+            "The request with a long address should fail",
         )
         self.assertDictEqual(
             response.json(),
@@ -165,7 +164,7 @@ class UserCreationTestCase(APITestCase):
         self.assertEqual(
             response.status_code,
             HTTP_400_BAD_REQUEST,
-            "The request with a short address shoul fail",
+            "The request with a wrong address should fail",
         )
         self.assertDictEqual(
             response.json(),
@@ -175,3 +174,109 @@ class UserCreationTestCase(APITestCase):
                 ]
             },
         )
+
+    def test_user_creation_short_signature(self):
+        """Checks sending a too short signature does not allow user creation"""
+
+        signature = "0x74cf934332732702e8f5906a09690b76ff90148f6ba5e014864961a027537e61e8df72ee1e564788219c956a6b84567e0a370da604207e9694f70b94fe141e11c"
+        address = "0xf17f52151EbEF6C7334FAD080c5704D77216b731"
+        timestamp = "2114380800"
+
+        response = self.client.post(
+            self.url,
+            data={"signature": signature, "address": address, "timestamp": timestamp},
+        )
+
+        self.assertEqual(
+            response.status_code,
+            HTTP_400_BAD_REQUEST,
+            "The request with a short signature should fail",
+        )
+        self.assertDictEqual(
+            response.json(),
+            {
+                settings.REST_FRAMEWORK["NON_FIELD_ERRORS_KEY"]: [
+                    errors.Signature.SHORT_SIGNATURE_ERROR
+                ]
+            },
+        )
+
+    def test_user_creation_long_signature(self):
+        """Checks sending a too long signature does not allow user creation"""
+
+        signature = "0x7400cf934332732702e8f5906a09690b76ff90148f6ba5e014864961a027537e61e8df72ee1e564788219c956a6b84567e0a370da604207e9694f70b94fe141e11c"
+        address = "0xf17f52151EbEF6C7334FAD080c5704D77216b731"
+        timestamp = "2114380800"
+
+        response = self.client.post(
+            self.url,
+            data={"signature": signature, "address": address, "timestamp": timestamp},
+        )
+
+        self.assertEqual(
+            response.status_code,
+            HTTP_400_BAD_REQUEST,
+            "The request with a too long signature should fail",
+        )
+        self.assertDictEqual(
+            response.json(),
+            {
+                settings.REST_FRAMEWORK["NON_FIELD_ERRORS_KEY"]: [
+                    errors.Signature.LONG_SIGNATURE_ERROR
+                ]
+            },
+        )
+
+    
+    def test_user_creation_wrong_signature(self):
+        """Checks sending a wrong signature does not allow user creation"""
+
+        signature = "0x74zcf934332732702e8f5906a09690b76ff90148f6ba5e014864961a027537e61e8df72ee1e564788219c956a6b84567e0a370da604207e9694f70b94fe141e11c"
+        address = "0xf17f52151EbEF6C7334FAD080c5704D77216b731"
+        timestamp = "2114380800"
+
+        response = self.client.post(
+            self.url,
+            data={"signature": signature, "address": address, "timestamp": timestamp},
+        )
+
+        self.assertEqual(
+            response.status_code,
+            HTTP_400_BAD_REQUEST,
+            "The request with a wrong signature should fail",
+        )
+        self.assertDictEqual(
+            response.json(),
+            {
+                settings.REST_FRAMEWORK["NON_FIELD_ERRORS_KEY"]: [
+                    errors.Signature.WRONG_SIGNATURE_ERROR
+                ]
+            },
+        )
+
+    def test_user_creation_mismatch_signature(self):
+        """Checks sending a mismatch signature does not allow user creation"""
+
+        signature = "0x741cf934332732702e8f5906a09690b76ff90148f6ba5e014864961a027537e61e8df72ee1e564788219c956a6b84567e0a370da604207e9694f70b94fe141e11c"
+        address = "0xf17f52151EbEF6C7334FAD080c5704D77216b731"
+        timestamp = "2114380800"
+
+        response = self.client.post(
+            self.url,
+            data={"signature": signature, "address": address, "timestamp": timestamp},
+        )
+
+        self.assertEqual(
+            response.status_code,
+            HTTP_400_BAD_REQUEST,
+            "The request with a mismatch signature should fail",
+        )
+        self.assertDictEqual(
+            response.json(),
+            {
+                settings.REST_FRAMEWORK["NON_FIELD_ERRORS_KEY"]: [
+                    errors.Signature.SIGNATURE_MISMATCH_ERROR
+                ]
+            },
+        )
+

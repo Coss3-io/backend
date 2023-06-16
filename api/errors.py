3d0c120d3351fbba1fd3b04706a13d01c3df3142
sylar123abc <sylar123abc@gmail.com>
2023-06-16 10:52:52 +0200

    Adding user related error messages

---
+++ api/errors.py
@@ -0,0 +1,5 @@
+# User view error messages
+USER_TIMESTAMP_ERROR = "The submitted timestamp for account creation is too old"
+
+# Signature Related Errors
+SIGNATURE_MISMATCH_ERROR = "The signature provided does not match the address"

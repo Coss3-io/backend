d9133ff8cf44c04acb8b46f0df3d329a726e0467
sylar123abc <sylar123abc@gmail.com>
2023-06-16 10:36:54 +0200

    Debugging the user view for working with tests

--- api/views/user.py
+++ api/views/user.py
@@ -1,17 +1,19 @@
 from time import time
 from adrf.views import APIView
 from asgiref.sync import sync_to_async
-from rest_framework.exceptions import APIException
 from rest_framework.status import HTTP_200_OK
 from rest_framework.response import Response
 from api.models import User
-from api.utils import validate_decimal_integer, validate_eth_signed_message
+from api.utils import validate_decimal_integer
 from api.models.types import Signature, Address
 from api.serializers.user import UserSerializer
 
 
 class UserView(APIView):
     """View for user creation"""
+
+    def get_queryset(self):
+        return User.objects.all()
 
     async def post(self, request):
         """View for user creation
@@ -25,12 +27,16 @@ class UserView(APIView):
         ```
         """
 
-        timestamp = validate_decimal_integer(request.get("timestamp", 0), "timestamp")
+        timestamp = validate_decimal_integer(request.data.get("timestamp", 0), "timestamp")
         signature = Signature(request.data.get("signature", ""))
         address = Address(request.data.get("address", ""))
 
         user = UserSerializer(
-            data={"signature": signature, "timestamp": timestamp, "address": address}  # type: ignore
+            data={"address": address},  # type: ignore
+            context={
+                "signature": signature,
+                "timestamp": int(timestamp),
+            },
         )
         await sync_to_async(user.is_valid)(raise_exception=True)
         await user.save()

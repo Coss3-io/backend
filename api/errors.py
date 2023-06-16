26136135180ccd8a688c60623d5e71754e4720b6
sylar123abc <sylar123abc@gmail.com>
2023-06-16 11:12:48 +0200

    creating error messages file

--- api/errors.py
+++ api/errors.py
@@ -1,5 +1,12 @@
 # User view error messages
-USER_TIMESTAMP_ERROR = "The submitted timestamp for account creation is too old"
+class User:
+    USER_TIMESTAMP_ERROR = "The submitted timestamp for account creation is too old"
 
 # Signature Related Errors
-SIGNATURE_MISMATCH_ERROR = "The signature provided does not match the address"
+class Signature:
+    SIGNATURE_MISMATCH_ERROR = "The signature provided does not match the address"
+
+# Number related Errors 
+class Decimal:
+    ZERO_DECIMAL_ERROR = "the {} submitted cannot be zero"
+    WRONG_DECIMAL_ERROR = "the {} submitted is not a number"

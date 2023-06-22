3d3aaeb50e175dab9896d40d608019a2f290f1ba
sylar123abc <sylar123abc@gmail.com>
2023-06-22 13:02:04 +0200

    adding watch tower permission to stacking view

--- api/views/stacking.py
+++ api/views/stacking.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import F
 from rest_framework import status
 from rest_framework.response import Response
@@ -7,9 +6,9 @@ from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import APIException
 from adrf.views import APIView
-from asgiref.sync import sync_to_async
 from api.models import User
 from api.models.stacking import Stacking, StakingFees
+from api.views.permissions import WatchTowerPermission
 from api.serializers.stacking import StackingSerializer, StackingFeesSerializer
 from api.models.types import Address
 from api.utils import validate_decimal_integer
@@ -22,7 +21,6 @@ class StackingView(APIView):
         data = super().get_permissions()
         return data + [permission() for permission in getattr(getattr(self, self.request.method.lower(), self.http_method_not_allowed), "permission_classes", [])]  # type: ignore
 
-
     @permission_classes([IsAuthenticated])
     async def get(self, request: Request):
         """Retrieves a user stacking amount per block"""
@@ -31,7 +29,7 @@ class StackingView(APIView):
         stackings = StackingSerializer(stackings, many=True)
         return Response(stackings.data, status=status.HTTP_200_OK)
 
-    @permission_classes([])
+    @permission_classes([WatchTowerPermission])
     async def post(self, request):
         """Allows the watch tower to publish new stacking entries
 
@@ -40,26 +38,20 @@ class StackingView(APIView):
         request.data = {
             "address": "0x123...",
             "amount": int,
-            "token": "0xabc...",
             "slot": int,
         }
         ```"""
-
-        if address := request.data.get("address", "") == "":
-            Address(address)
+        if (address := request.data.get("address", "")) == "":
             raise APIException("address can't be empty")
-
-        if token := request.data.get("token", "") == "":
-            Address(token, "token")
-            raise APIException("address can't be empty")
+        Address(address)
 
         amount = validate_decimal_integer(request.data.get("amount", "0"), "amount")
         slot = validate_decimal_integer(request.data.get("slot", "0"), "slot")
 
-        user = await User.objects.aget_or_create(address=request.data.get("address"))
-        stacking = (
-            await Stacking.objects.aget_or_create(user=user, token=token, slot=slot)
-        )[0]
+        user = (await User.objects.aget_or_create(address=request.data.get("address")))[
+            0
+        ]
+        stacking = (await Stacking.objects.aget_or_create(user=user, slot=slot))[0]
 
         stacking.amount = F("amount") + amount
         await stacking.asave(update_fields=["amount"])
@@ -73,7 +65,6 @@ class StackingFeesView(APIView):
         data = super().get_permissions()
         return data + [permission() for permission in getattr(getattr(self, self.request.method.lower(), self.http_method_not_allowed), "permission_classes", [])]  # type: ignore
 
-
     async def get(self, request):
         """Used to get the fees entries for all the slots since the creation of the app"""
 
@@ -81,7 +72,7 @@ class StackingFeesView(APIView):
         data = StackingFeesSerializer(stacking_fees, many=True)
         return Response(data, status=status.HTTP_200_OK)
 
-    @permission_classes([])
+    @permission_classes([WatchTowerPermission])
     async def post(self, request):
         """Used by the watch tower to commit new stacking fees entries
 

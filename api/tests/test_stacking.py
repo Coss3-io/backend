78fd62fba689363961a9323ba49593c49ae47280
sylar123abc <sylar123abc@gmail.com>
2023-06-23 13:27:43 +0200

    Adding stacking tests

--- api/tests/test_stacking.py
+++ api/tests/test_stacking.py
@@ -6,10 +6,11 @@ from asgiref.sync import async_to_sync
 from django.urls import reverse
 from django.conf import settings
 from rest_framework.test import APITestCase
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
 from api.models.stacking import Stacking
 from api.models.types import Address
 from api.models import User
+import api.errors as errors
 
 
 class StackingTestCase(APITestCase):
@@ -17,11 +18,14 @@ class StackingTestCase(APITestCase):
     stacking entries and the retrieval of the existing ones
     """
 
-    def test_stacking_entries_creation_from_wt_work(self):
-        """Checks stacking entries creation works well"""
+    def setUp(self) -> None:
         async_to_sync(User.objects.create_user)(
             address=Address("0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef")
         )
+
+    def test_stacking_entries_creation_from_wt_work(self):
+        """Checks stacking entries creation works well"""
+
         data = {
             "address": "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef",
             "amount": "{0:f}".format(Decimal("173e16")),
@@ -55,3 +59,177 @@ class StackingTestCase(APITestCase):
             Decimal(data["slot"]),
             "The slot of the stacking entry should match the slot sent",
         )
+
+    def test_stacking_creation_wrong_signature_fails(self):
+        """Checks a stacking entry creation with wrong signature
+        fails
+        """
+
+        data = {
+            "address": "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef",
+            "amount": "{0:f}".format(Decimal("173e16")),
+            "slot": "23",
+        }
+
+        data["timestamp"] = str(int(time()) * 1000)
+        data["signature"] = hmac.new(
+            key=settings.WATCH_TOWER_KEY.encode(),
+            msg=dumps(data).encode(),
+            digestmod="sha256",
+        ).hexdigest()
+        data["signature"] = data["signature"][:-5] + "aaaaa"
+
+        response = self.client.post(reverse("api:stacking"), data=data)
+
+        self.assertDictEqual(
+            response.json(),
+            {"detail": errors.Permissions.WATCH_TOWER_AUTH_FAIL},
+            "The stacking entry creation should fail with a wrong signature",
+        )
+
+        self.assertEqual(
+            response.status_code,
+            HTTP_403_FORBIDDEN,
+            "The request should not be allowed with a wrong signature",
+        )
+
+    def test_stacking_user_creation_new_entry(self):
+        """Checks if the user does not exist it is created
+        on new stacking entry
+        """
+
+        data = {
+            "address": "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fea",
+            "amount": "{0:f}".format(Decimal("173e16")),
+            "slot": "23",
+        }
+
+        data["timestamp"] = str(int(time()) * 1000)
+        data["signature"] = hmac.new(
+            key=settings.WATCH_TOWER_KEY.encode(),
+            msg=dumps(data).encode(),
+            digestmod="sha256",
+        ).hexdigest()
+
+        response = self.client.post(reverse("api:stacking"), data=data)
+
+        self.assertEqual(
+            response.status_code,
+            HTTP_200_OK,
+            "The request with a non existing user should work",
+        )
+
+        User.objects.get(address=data["address"])
+
+    def test_stacking_entry_update(self):
+        """Checks updating a stacking entry works"""
+
+        data = {
+            "address": "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef",
+            "amount": "{0:f}".format(Decimal("193e16")),
+            "slot": "23",
+        }
+
+        data["timestamp"] = str(int(time()) * 1000)
+        data["signature"] = hmac.new(
+            key=settings.WATCH_TOWER_KEY.encode(),
+            msg=dumps(data).encode(),
+            digestmod="sha256",
+        ).hexdigest()
+
+        response = self.client.post(reverse("api:stacking"), data=data)
+        stack_entry = Stacking.objects.get(
+            user__address=data["address"], slot=data["slot"]
+        )
+
+        self.assertEqual(
+            stack_entry.amount,
+            Decimal(data["amount"]),
+            "The amount of the stacking entry should be updated",
+        )
+
+        self.assertDictEqual(
+            response.json(), {}, "No data should be returned on success"
+        )
+
+        self.assertEqual(
+            response.status_code, HTTP_200_OK, "The stacking entry update should work"
+        )
+
+    def test_stacking_entry_creation_wrong_address_fails(self):
+        """Checks creating a stacking entry with a wrong address fails"""
+
+        data = {
+            "address": "0xZ5fdf4076b8F3A5357c5E395ab970B5B54098Fef",
+            "amount": "{0:f}".format(Decimal("193e16")),
+            "slot": "23",
+        }
+
+        data["timestamp"] = str(int(time()) * 1000)
+        data["signature"] = hmac.new(
+            key=settings.WATCH_TOWER_KEY.encode(),
+            msg=dumps(data).encode(),
+            digestmod="sha256",
+        ).hexdigest()
+
+        response = self.client.post(reverse("api:stacking"), data=data)
+
+        self.assertEqual(
+            response.status_code,
+            HTTP_400_BAD_REQUEST,
+            "The request should fail with wrong address",
+        )
+
+        self.assertDictEqual(
+            response.json(),
+            {"address": [errors.Address.WRONG_ADDRESS_ERROR.format("")]},
+        )
+
+    def test_stacking_entry_creation_empty_address(self):
+        """Checks stacking entry creation with empty address fails"""
+
+        data = {
+            # "address": "0xZ5fdf4076b8F3A5357c5E395ab970B5B54098Fef",
+            "amount": "{0:f}".format(Decimal("193e16")),
+            "slot": "23",
+        }
+
+        data["timestamp"] = str(int(time()) * 1000)
+        data["signature"] = hmac.new(
+            key=settings.WATCH_TOWER_KEY.encode(),
+            msg=dumps(data).encode(),
+            digestmod="sha256",
+        ).hexdigest()
+
+        response = self.client.post(reverse("api:stacking"), data=data)
+
+        self.assertEqual(
+            response.status_code,
+            HTTP_400_BAD_REQUEST,
+            "The request should fail with empty address",
+        )
+
+        self.assertDictEqual(
+            response.json(),
+            {"address": [errors.General.MISSING_FIELD]},
+        )
+
+    def test_stacking_entry_creation_wrong_slot(self):
+        """Checks stacking creation with a wrong slot does not work"""
+
+        data = {
+            "address": "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef",
+            "amount": "{0:f}".format(Decimal("193e16")),
+            "slot": "a23",
+        }
+
+        data["timestamp"] = str(int(time()) * 1000)
+        data["signature"] = hmac.new(
+            key=settings.WATCH_TOWER_KEY.encode(),
+            msg=dumps(data).encode(),
+            digestmod="sha256",
+        ).hexdigest()
+
+        response = self.client.post(reverse("api:stacking"), data=data)
+
+        #self.assertDictEqual(response.json(), ) 

cf4908b3191ebc836162e8da0877832da354fe4f
sylar123abc <sylar123abc@gmail.com>
2023-06-23 13:28:05 +0200

    Adding stacking serializer field verification

--- api/serializers/stacking.py
+++ api/serializers/stacking.py
@@ -1,14 +1,30 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from api.models.stacking import Stacking, StakingFees
+from api.utils import validate_address, validate_decimal_integer
 
 
 class StackingSerializer(ModelSerializer):
     """Class used to serialize user stacking entries"""
 
+    address = CharField(required=True, allow_blank=False, write_only=True)
+
+    async def create(self, validated_data):
+        del validated_data["amount"]
+        del validated_data["address"]
+        return (await Stacking.objects.aget_or_create(**validated_data))[0]
+
     class Meta:
         model = Stacking
-        fields = ["token", "slot"]
-        extra_kwargs = {"user": {"write_only": True}}
+        fields = ["slot", "amount", "address"]
+
+    def validate_slot(self, value):
+        return validate_decimal_integer(value, "slot")
+
+    def validate_amount(self, value):
+        return validate_decimal_integer(value, "amount")
+
+    def validate_address(self, value):
+        return validate_address(value, "")
 
 
 class StackingFeesSerializer(ModelSerializer):

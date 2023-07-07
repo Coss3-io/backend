7d443ae49a28bcb6dc1c3d0b660bb4ef65c665e5
sylar123abc <sylar123abc@gmail.com>
2023-07-07 14:40:36 +0200

    adding the user log in view

--- api/views/user.py
+++ api/views/user.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from api.models import User
 from api.utils import validate_eth_signed_message
-from api.models.types import Address, KeccakHash
+from api.models.types import Address, Signature
 from api.serializers.user import UserSerializer
 import api.errors as errors
 
@@ -61,25 +61,32 @@ class UserLogInView(APIView):
     async def post(self, request):
         """Method used for a user to log in into the app"""
 
-        address = request.get("address", "")
-        timestamp = request.get("timestamp", 0)
-        signature = request.get("signature", "")
+        field_errors = {}
+        if (address := request.data.get("address", "")) == "":
+            field_errors["address"] = [errors.General.MISSING_FIELD]
+        if (timestamp := request.data.get("timestamp", "")) == "":
+            field_errors["timestamp"] = [errors.General.MISSING_FIELD]
+        if (signature := request.data.get("signature", "")) == "":
+            field_errors["signature"] = [errors.General.MISSING_FIELD]
 
-        if int(time()) - int(timestamp) < 5000:
+        if field_errors:
+            return Response(field_errors, status=HTTP_400_BAD_REQUEST)
+
+        if int(time()) - int(timestamp) > 5000:
             return Response(
                 {"error": [errors.General.TOO_OLD_TIMESTAMP]},
                 status=HTTP_400_BAD_REQUEST,
             )
-        
+
         try:
             checksum_address = Address(address)
-            KeccakHash(signature)
+            Signature(signature)
         except ValidationError as e:
             return Response(
-                {"error": [e.detail]},
+                {"error": e.detail},
                 status=HTTP_400_BAD_REQUEST,
             )
-        
+
         if checksum_address != address:
             return Response(
                 {"error": [errors.General.CHECKSUM_ADDRESS_NEEDED]},
@@ -95,18 +102,12 @@ class UserLogInView(APIView):
                 address=address,
             )
             == False
-        ):  
+        ):
             return Response(
                 {"error": [errors.Signature.SIGNATURE_MISMATCH_ERROR]},
                 status=HTTP_400_BAD_REQUEST,
             )
-        
+
         user = (await User.objects.aget_or_create(address=checksum_address))[0]
-        login(request=request, user=user)
+        await sync_to_async(login)(request=request, user=user)
         return Response({}, status=HTTP_200_OK)
-
-
-
-        
-
-        

d48b2e75371b22fa66cb3e0cf30ab1b3b074b7da
sylar123abc <sylar123abc@gmail.com>
2023-07-07 14:40:03 +0200

    Adding the login endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from api.views.orders import OrderView, MakerView, BotView
 from api.views.watch_tower import WatchTowerView
-from api.views.user import UserCreateView
+from api.views.user import UserCreateView, UserLogInView
 from api.views.stacking import StackingView, StackingFeesView
 
 app_name = "api"
 urlpatterns = [
     path("acccount", UserCreateView.as_view(), name="account"),
+    path("login", UserLogInView.as_view(), name="login"),
     path("orders", OrderView.as_view(), name="orders"),
     path("order", MakerView.as_view(), name="order"),
     path("stacking", StackingView.as_view(), name="stacking"),

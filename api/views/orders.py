c2b50d8b15c5437b20e8873544eef5f28b9762c3
sylar123abc <sylar123abc@gmail.com>
2023-07-10 12:49:34 +0200

    Adding API auth for orders

--- api/views/orders.py
+++ api/views/orders.py
@@ -4,15 +4,17 @@ from asgiref.sync import sync_to_async
 from adrf.views import APIView
 from django.db.utils import IntegrityError
 from rest_framework import status
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
 from api.models.orders import Maker, Bot
 from api.models.types import Address
+from api.models import User
 import api.errors as errors
 from api.serializers.orders import MakerSerializer, BotSerializer
+from api.views.authentications import ApiAuthentication
 
 
 class OrderView(APIView):
@@ -47,13 +49,21 @@ class OrderView(APIView):
 class MakerView(APIView):
     """The views user to retrieve and create Maker Orders"""
 
+    def get_authenticators(self):
+        data = super().get_authenticators()
+        return data + [auth() for auth in getattr(getattr(self, self.request.method.lower(), self.http_method_not_allowed), "authentication_classes", [])]  # type: ignore
+
     def get_permissions(self):
         data = super().get_permissions()
         return data + [permission() for permission in getattr(getattr(self, self.request.method.lower(), self.http_method_not_allowed), "permission_classes", [])]  # type: ignore
 
+    @authentication_classes([ApiAuthentication])
     @permission_classes([IsAuthenticated])
     async def get(self, request: Request):
         """The view to retrieve the orders of a user"""
+        if request.auth == "awaitable":
+            request.user = (await User.objects.aget_or_create(address=request.user))[0]
+
         if request.query_params.get("all", None):
             queryset = Maker.objects.filter(user=request.user).select_related("user")
         else:
@@ -96,13 +106,21 @@ class MakerView(APIView):
 class BotView(APIView):
     """View used to create and retrieve bots"""
 
+    def get_authenticators(self):
+        data = super().get_authenticators()
+        return data + [auth() for auth in getattr(getattr(self, self.request.method.lower(), self.http_method_not_allowed), "authentication_classes", [])]  # type: ignore
+
     def get_permissions(self):
         data = super().get_permissions()
         return data + [permission() for permission in getattr(getattr(self, self.request.method.lower(), self.http_method_not_allowed), "permission_classes", [])]  # type: ignore
 
+    @authentication_classes([ApiAuthentication])
     @permission_classes([IsAuthenticated])
     async def get(self, request):
         """Returns the user bots list,"""
+        if request.auth == "awaitable":
+            request.user = (await User.objects.aget_or_create(address=request.user))[0]
+
         bots = Bot.objects.filter(user=request.user).prefetch_related("orders")
         data = await sync_to_async(lambda: BotSerializer(bots, many=True).data)()
         return Response(data, status=status.HTTP_200_OK)

6ef53b2f08aba607b4cb0d59e63abe0ccdf9c4f3
sylar123abc <sylar123abc@gmail.com>
2023-07-10 12:49:06 +0200

    reaafactoring user log in function

--- api/views/user.py
+++ api/views/user.py
@@ -9,7 +9,11 @@ from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from api.models import User
-from api.utils import validate_eth_signed_message, validate_decimal_integer
+from api.utils import (
+    validate_eth_signed_message,
+    validate_decimal_integer,
+    validate_user,
+)
 from api.models.types import Address, Signature
 from api.serializers.user import UserSerializer
 import api.errors as errors
@@ -61,54 +65,10 @@ class UserLogInView(APIView):
     async def post(self, request):
         """Method used for a user to log in into the app"""
 
-        field_errors = {}
-        if (address := request.data.get("address", "")) == "":
-            field_errors["address"] = [errors.General.MISSING_FIELD]
-        if (timestamp := request.data.get("timestamp", "")) == "":
-            field_errors["timestamp"] = [errors.General.MISSING_FIELD]
-        if (signature := request.data.get("signature", "")) == "":
-            field_errors["signature"] = [errors.General.MISSING_FIELD]
+        success, result = validate_user(request, settings.LOG_IN_MESSAGE)
 
-        if field_errors:
-            return Response(field_errors, status=HTTP_400_BAD_REQUEST)
-
-        try:
-            validate_decimal_integer(timestamp, "timestamp")
-            checksum_address = Address(address)
-            Signature(signature)
-        except ValidationError as e:
-            return Response(
-                {"error": e.detail},
-                status=HTTP_400_BAD_REQUEST,
-            )
-        
-        if int(time()) - int(timestamp) > 5000:
-            return Response(
-                {"timestamp": [errors.General.TOO_OLD_TIMESTAMP]},
-                status=HTTP_400_BAD_REQUEST,
-            )
-
-        if checksum_address != address:
-            return Response(
-                {"address": [errors.General.CHECKSUM_ADDRESS_NEEDED]},
-                status=HTTP_400_BAD_REQUEST,
-            )
-
-        message = settings.LOG_IN_MESSAGE.format(address=address, timestamp=timestamp)
-
-        if (
-            validate_eth_signed_message(
-                message=message.encode(),
-                signature=signature,
-                address=address,
-            )
-            == False
-        ):
-            return Response(
-                {"signature": [errors.Signature.SIGNATURE_MISMATCH_ERROR]},
-                status=HTTP_400_BAD_REQUEST,
-            )
-
-        user = (await User.objects.aget_or_create(address=checksum_address))[0]
+        if not success:
+            return Response(result, status=HTTP_400_BAD_REQUEST)
+        user = (await User.objects.aget_or_create(address=result))[0]
         await sync_to_async(login)(request=request, user=user)
         return Response({}, status=HTTP_200_OK)

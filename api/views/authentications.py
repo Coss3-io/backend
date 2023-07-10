75ca1a9c19144cd427e09fc8219fb84a1d8d7591
sylar123abc <sylar123abc@gmail.com>
2023-07-10 13:43:13 +0200

    Updating the auth system

--- api/views/authentications.py
+++ api/views/authentications.py
@@ -6,15 +6,15 @@ from api.utils import validate_user
 
 class ApiAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
-        if not request.data.get("signature", ""):
+        if not request.query_params.get("signature", ""):
             return None
         success, result = validate_user(
-            request,
+            request.query_params,
             settings.API_LOG_IN_MESSAGE.format(
                 method=request.method, path=request.path
             ),
         )
-
         if not success:
             raise exceptions.AuthenticationFailed(result)
-        return result, "await"
+        result.__setattr__("is_authenticated",True) 
+        return result, "awaitable"

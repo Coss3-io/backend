c50b1ad5ce9fc4f39c51e7a03cf116fcbd75e6be
sylar123abc <sylar123abc@gmail.com>
2023-07-12 13:56:22 +0200

    Updating watch tower commit function

--- api/views/watch_tower.py
+++ api/views/watch_tower.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
 from typing import Any
 from asgiref.sync import sync_to_async
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import F
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
@@ -8,10 +10,10 @@ from adrf.views import APIView
 from api.models import User
 import api.errors as errors
 from api.models.types import Address
-from api.utils import validate_decimal_integer
+from api.utils import validate_decimal_integer, compute_order_hash
 from api.views.permissions import WatchTowerPermission
-from api.models.orders import Maker
-from api.serializers.orders import TakerSerializer
+from api.models.orders import Maker, Bot
+from api.serializers.orders import TakerSerializer, MakerSerializer
 
 
 class WatchTowerView(APIView):
@@ -42,13 +44,13 @@ class WatchTowerView(APIView):
                 {"trade": [errors.General.MISSING_FIELD.format("trade")]},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        
+
         if not (block := request.data.get("block", False)):
             return Response(
                 {"block": [errors.General.MISSING_FIELD.format("block")]},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        
+
         if not isinstance(trades, dict):
             return Response(
                 {"trades": [errors.Order.TRADE_FIELD_FORMAT_ERROR]},
@@ -69,14 +71,20 @@ class WatchTowerView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
+        if not makers_hash_list:
+            pass # add an error for non existent maker hashes
+
         takers: Any = []
         user = User.objects.aget_or_create(address=checksum_address)
-        makers = Maker.objects.filter(order_hash__in=[makers_hash_list])
+        makers = Maker.objects.filter(order_hash__in=makers_hash_list).select_related(
+            "bot"
+        )
+        bot_update = []
 
         async for maker in makers:
             takers.append(
                 {
-                    "maker": maker,
+                    "maker_id": maker.id,
                     "block": block,
                     "taker_amount": trades[maker.order_hash]["taker_amount"],
                     "fees": trades[maker.order_hash]["fees"],
@@ -84,12 +92,75 @@ class WatchTowerView(APIView):
                     "base_fees": trades[maker.order_hash]["base_fees"],
                 }
             )
+            
+
+            if maker.bot is not None:
+                if maker.is_buyer == trades[maker.order_hash]["is_buyer"]:
+                    maker.filled = (
+                        F("filled") - trades[maker.order_hash]["taker_amount"]
+                    )
+                    if maker.is_buyer:
+                        if maker.bot.maker_fees > Decimal("2000"):
+                            fees = maker.bot.maker_fees * maker.amount
+                        else:
+                            fees = (
+                                (maker.price * maker.bot.maker_fees + Decimal("1000"))
+                                / (Decimal("1000"))
+                                - maker.price
+                            ) * maker.amount
+                    else:
+                        if maker.bot.maker_fees > Decimal("2000"):
+                            fees = maker.bot.maker_fees * maker.amount
+                        else:
+                            fees = (
+                                maker.price
+                                - (maker.price * Decimal("1000"))
+                                / (maker.bot.maker_fees + Decimal("1000"))
+                            ) * maker.amount
+                else:
+                    maker.filled = (
+                        F("filled") + trades[maker.order_hash]["taker_amount"]
+                    )
+                    if maker.is_buyer:
+                        if maker.bot.maker_fees > Decimal("2000"):
+                            fees = maker.bot.maker_fees * maker.amount
+                        else:
+                            fees = (
+                                maker.price
+                                - (maker.price * Decimal("1000"))
+                                / (maker.bot.maker_fees + Decimal("1000"))
+                            ) * maker.amount
+                    else:
+                        if maker.bot.maker_fees > Decimal("2000"):
+                            fees = maker.bot.maker_fees * maker.amount
+                        else:
+                            fees = (
+                                (maker.price * maker.bot.maker_fees + Decimal("1000"))
+                                / (Decimal("1000"))
+                                - maker.price
+                            ) * maker.amount
+                            
+                maker.bot.fees_earned = F("fees_earned") + fees
+                bot_update.append(maker.bot)
+            else:
+                if (
+                    maker.filled + Decimal(trades[maker.order_hash]["taker_amount"])
+                    == maker.amount
+                ):
+                    maker.filled = maker.amount
+                    maker.status = Maker.FILLED
+                else:
+                    maker.filled = F("filled") + trades[maker.order_hash]["taker_amount"]
 
         takers_serializer = TakerSerializer(data=takers, many=True)
         await sync_to_async(takers_serializer.is_valid)(raise_exception=True)
         takers_serializer.save(user=(await user)[0])
         if takers_serializer.instance:
             await takers_serializer.instance
+        await Maker.objects.abulk_update(makers, ["filled", "status"]) #type: ignore
+
+        if bot_update:
+            await Bot.objects.abulk_update(bot_update, ["fees_earned"]) #type: ignore
         return Response({}, status=status.HTTP_200_OK)
 
     async def delete(self, request):

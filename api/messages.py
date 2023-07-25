fbef7d4e02e4379e207c286670eed94fba8188a2
sylar123abc <sylar123abc@gmail.com>
2023-07-25 13:53:56 +0200

    fixing websocket frame typo

--- api/messages.py
+++ api/messages.py
@@ -4,6 +4,6 @@ class WStypes:
     NEW_BOT = "new_bot"
     NEW_STACKING = "new_stacking"
     NEW_FEES = "new_fees"
-    MAKERS_UPDATE = "makers_update",
+    MAKERS_UPDATE = "makers_update"
     BOT_FEES = "bot_fees"
     DEL_MAKER = "delete_maker"

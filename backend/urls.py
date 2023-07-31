77d2a0c03a517b02bea326f366189a956a2d3fa6
sylar123abc <sylar123abc@gmail.com>
2023-07-31 10:58:53 +0200

    redirecting the admin log in to the regular log in

--- backend/urls.py
+++ backend/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('admin/login/', RedirectView.as_view(url='/'), name='login'),
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
 ]

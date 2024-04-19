253e3478e9455b3789eff00200fee8571b44aa93
sylar123abc <sylar123abc@gmail.com>
2024-04-19 12:31:53 +0200

    Adding testing websockets routes

--- backend/asgi.py
+++ backend/asgi.py
@@ -12,17 +12,37 @@ import os
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 from channels.routing import ProtocolTypeRouter, URLRouter
-from api.consumers.websocket import WebsocketConsumer
+from api.consumers.websocket import WebsocketConsumer, TestWebsocketConsumer
+from backend import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-django_asgi_app = get_asgi_application()
-ws_asgi_app = URLRouter(
-    [
-        re_path(r"^ws/stacking/(?P<chain_id>\w+)$", WebsocketConsumer.as_asgi()),
-        re_path(r"^ws/trade/(?P<chain_id>\w+)/(?P<base>\w+)/(?P<quote>\w+)$", WebsocketConsumer.as_asgi()),
-    ],
+routes = [
+    re_path(r"^ws/stacking/(?P<chain_id>\w+)$", WebsocketConsumer.as_asgi()),
+    re_path(
+        r"^ws/trade/(?P<chain_id>\w+)/(?P<base>\w+)/(?P<quote>\w+)$",
+        WebsocketConsumer.as_asgi(),
+    ),
+]
+
+routes += (
+    [re_path(r"^ws/test/stacking/(?P<chain_id>\w+)$", TestWebsocketConsumer.as_asgi())]
+    if settings.DEBUG
+    else []
 )
+routes += (
+    [
+        re_path(
+            r"^ws/test/trade/(?P<chain_id>\w+)/(?P<base>\w+)/(?P<quote>\w+)$",
+            TestWebsocketConsumer.as_asgi(),
+        )
+    ]
+    if settings.DEBUG
+    else []
+)
+
+django_asgi_app = get_asgi_application()
+ws_asgi_app = URLRouter(routes)
 
 application = ProtocolTypeRouter(
     {
